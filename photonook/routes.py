8a13c89f4aebc872c5b97e5c94209c2841d60e91
mihmilicio <milenamilicio@hotmail.com>
2023-11-21 16:07:08 -0300

    configura profile

--- photonook/routes.py
+++ photonook/routes.py
@@ -3,6 +3,7 @@ import os
 from flask import render_template, redirect, url_for
 from flask_login import login_required, login_user, logout_user, current_user
 from werkzeug.utils import secure_filename
+from datetime import datetime, date
 
 from photonook.models import User, Post
 from photonook import app, database
@@ -48,14 +49,14 @@ def login():
 @login_required
 def home():
   _formNewPost = FormCreateNewPost()
+  _basedir = os.path.abspath(os.path.dirname(__file__))
 
   if _formNewPost.validate_on_submit():
     _post_text = _formNewPost.text.data
+
     _post_img = _formNewPost.photo.data
     _img_name = secure_filename(_post_img.filename)
-    path = os.path.abspath(os.path.dirname(__file__))
-    path2 = app.config['UPLOAD_FOLDER']
-    _final_path = f'{path}/{path2}/{_img_name}'
+    _final_path = os.path.join(_basedir, app.config['UPLOAD_FOLDER'], _img_name)
 
     _post_img.save(_final_path)
 
@@ -64,32 +65,41 @@ def home():
                     user_id=int(current_user.id)
                     )
 
-    # salvar no banco
     database.session.add(newPost)
     database.session.commit()
 
-  users = User.query.all()
+    return redirect(url_for('home'))
 
-  return render_template("home.html", users=users, user=None, form=_formNewPost, posts=current_user.posts)
+  users = User.query.all()
+  users.remove(current_user)
+  users.insert(0, current_user)
+
+  posts = Post.query.all()
+  posts.sort(key=lambda post: datetime.strptime(post.creation_date, '%Y-%m-%d %H:%M:%S.%f'), reverse=True)
+  new_posts = []
+
+  for post in posts:
+    new_post = post
+    new_post.post_img = os.path.join(app.config['UPLOAD_FOLDER'], post.post_img).replace("\\","/")
+    new_post.creation_date = datetime.strptime(post.creation_date, '%Y-%m-%d %H:%M:%S.%f').strftime("%d/%m/%Y at %H:%M")
+    new_post.username = User.query.get(int(post.user_id)).username
+    new_posts.append(new_post)
+
+  return render_template("home.html", users=users, user=None, form=_formNewPost, posts=new_posts)
 
 
 @app.route('/profile/<user_id>', methods=['POST', 'GET'])
 @login_required
 def profile(user_id):
   _formNewPost = FormCreateNewPost()
+  _basedir = os.path.abspath(os.path.dirname(__file__))
+
   if _formNewPost.validate_on_submit():
-      # pegar o texto
       _post_text = _formNewPost.text.data
 
-      # pegar a img
       _post_img = _formNewPost.photo.data
       _img_name = secure_filename(_post_img.filename)
-      _basedir = os.path.abspath(os.path.dirname(__file__))
-      print(_basedir)
-      path2 = app.config['UPLOAD_FOLDER']
-      print(path2)
       _final_path = os.path.join(_basedir, app.config['UPLOAD_FOLDER'], _img_name)
-      print(_final_path)
 
       _post_img.save(_final_path)
 
@@ -98,16 +108,29 @@ def profile(user_id):
                       user_id=int(current_user.id)
                       )
 
-      # salvar no banco
       database.session.add(newPost)
       database.session.commit()
 
+      return redirect(url_for('profile', user_id))
+
   _user = User.query.get(int(user_id))
+
   users = User.query.all()
   users.remove(current_user)
   users.insert(0, current_user)
 
+  posts = _user.posts
+  posts.sort(key=lambda post: datetime.strptime(post.creation_date, '%Y-%m-%d %H:%M:%S.%f'), reverse=True)
+  new_posts = []
+
+  for post in posts:
+    new_post = post
+    new_post.post_img = os.path.join(app.config['UPLOAD_FOLDER'], post.post_img).replace("\\","/")
+    new_post.creation_date = datetime.strptime(post.creation_date, '%Y-%m-%d %H:%M:%S.%f').strftime("%d/%m/%Y at %H:%M")
+    new_post.username = _user.username
+    new_posts.append(new_post)
+  
   if int(user_id) == int(current_user.id):
-    return render_template("home.html", users=users, user=_user, form=_formNewPost, posts=_user.posts)
+    return render_template("home.html", users=users, user=_user, form=_formNewPost, posts=posts)
   
   return render_template("home.html", users=users, user=_user, form=None, posts=_user.posts)
